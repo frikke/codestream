93287246e3b61089a733d72c22d607d2838331ac
eric-j <etjones319@gmail.com>
2022-07-21 13:03:42 -0700

    cleanup

--- shared/ui/Authentication/Signup.tsx
+++ shared/ui/Authentication/Signup.tsx
@@ -189,7 +189,7 @@ export const Signup = (props: Props) => {
 				selectedRegionName = selectedHost.name;
 			}
 			// Set to first value in array if non of the above for default
-		} else if (!selectedRegion && !forceRegion && environmentHosts.length > 0) {
+		} else {
 			selectedRegionName = environmentHosts[0]?.name;
 		}
 	}
