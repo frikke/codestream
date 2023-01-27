2466566668626e81377f0ae8138abf3edbdafbbf
eric-j <etjones319@gmail.com>
2023-01-27 11:22:31 -0800

    working prototype for maintennce mode polling

--- vscode/src/controllers/webviewController.ts
+++ vscode/src/controllers/webviewController.ts
@@ -19,6 +19,7 @@ import {
 	DidChangeVersionCompatibilityNotification,
 	DidChangeVersionCompatibilityNotificationType,
 	DidEncounterMaintenanceModeNotificationType,
+	RefreshMaintenancePollNotificationType,
 	DidResolveStackTraceLineNotificationType,
 	ReportingMessageType,
 	VersionCompatibility
@@ -156,6 +157,9 @@ export class WebviewController implements Disposable {
 			workspace.onDidChangeWorkspaceFolders(this.onWorkspaceFoldersChanged, this),
 			Container.agent.onDidEncounterMaintenanceMode(e => {
 				if (this._webview) this._webview.notify(DidEncounterMaintenanceModeNotificationType, e);
+			}),
+			Container.agent.onRefreshMaintenancePoll(e => {
+				if (this._webview) this._webview.notify(RefreshMaintenancePollNotificationType, e);
 			}),
 			Container.agent.onDidResolveStackTraceLine(e => {
 				if (this._webview) this._webview.notify(DidResolveStackTraceLineNotificationType, e);
