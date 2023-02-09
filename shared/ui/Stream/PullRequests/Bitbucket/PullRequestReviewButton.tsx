a394dd0961938889ecbb06725d24ffa930cb1dfa
Brian Canzanella <brian@codestream.com>
2023-02-09 15:31:47 -0500

    small changes

--- shared/ui/Stream/PullRequests/Bitbucket/PullRequestReviewButton.tsx
+++ shared/ui/Stream/PullRequests/Bitbucket/PullRequestReviewButton.tsx
@@ -3,6 +3,7 @@ import Icon from "../../Icon";
 import { api } from "../../../store/providerPullRequests/thunks";
 import { useAppDispatch, useDidMount } from "@codestream/webview/utilities/hooks";
 import { FetchThirdPartyPullRequestPullRequest } from "@codestream/protocols/agent";
+import Button from "../../Button";
 
 interface Props {
 	pullRequest: FetchThirdPartyPullRequestPullRequest;
@@ -12,93 +13,88 @@ export const PullRequestReviewButton = (props: Props) => {
 	const dispatch = useAppDispatch();
 
 	const mapping = {
-		APPROVED: { icon: "thumbsup", text: "Unapprove" },
-		UNAPPROVED: { icon: "thumbsdown", text: "Approve" },
-		REQUEST_CHANGES: { icon: "question", text: "Request Changes" },
-		CHANGES_REQUESTED: { icon: "question", text: "Changes Requested" },
+		APPROVED: { icon: "thumbsup", text: "Unapprove", requestedState: "UNAPPROVED" },
+		UNAPPROVED: { icon: "thumbsdown", text: "Approve", requestedState: "APPROVED" },
+		REQUEST_CHANGES: {
+			icon: "question",
+			text: "Request Changes",
+			requestedState: "REQUEST_CHANGES",
+		},
+		CHANGES_REQUESTED: {
+			icon: "question",
+			text: "Changes Requested",
+			requestedState: "CHANGES_REQUESTED",
+		},
 	};
 
 	const isApproved = props.pullRequest.isApproved;
 	const isRequested = props.pullRequest.isRequested;
 
 	useDidMount(() => {
-		display_logic();
-	});
-
-	const [reviewTypeApprovalIcon, setReviewTypeApprovalIcon] = useState("");
-	const [reviewTypeApprovalText, setReviewTypeApprovalText] = useState("");
-	const [reviewTypeRequestIcon, setReviewTypeRequestIcon] = useState("");
-	const [reviewTypeRequestText, setReviewTypeRequestText] = useState("");
-
-	const display_logic = () => {
 		if (!isApproved && !isRequested) {
 			// it's not approved and not requested, should show thumbsdown and text approve as well as question and Request Changes
-			setReviewTypeApprovalIcon(mapping["UNAPPROVED"].icon);
-			setReviewTypeApprovalText(mapping["UNAPPROVED"].text);
-			setReviewTypeRequestIcon(mapping["REQUEST_CHANGES"].icon);
-			setReviewTypeRequestText(mapping["REQUEST_CHANGES"].text);
+			setReviewTypeApproval(mapping["UNAPPROVED"]);
+			setReviewTypeRequest(mapping["REQUEST_CHANGES"]);
 		} else if (!isApproved && isRequested) {
 			//it's not aproved but changes are requested, should show thumbs down and text approve as well as question and Changes Requested
-			setReviewTypeApprovalIcon(mapping["UNAPPROVED"].icon);
-			setReviewTypeApprovalText(mapping["UNAPPROVED"].text);
-			setReviewTypeRequestIcon(mapping["CHANGES_REQUESTED"].icon);
-			setReviewTypeRequestText(mapping["CHANGES_REQUESTED"].text);
+			setReviewTypeApproval(mapping["UNAPPROVED"]);
+			setReviewTypeRequest(mapping["CHANGES_REQUESTED"]);
 		} else if (isApproved) {
-			setReviewTypeApprovalIcon(mapping["APPROVED"].icon);
-			setReviewTypeApprovalText(mapping["APPROVED"].text);
-			setReviewTypeRequestIcon(mapping["REQUEST_CHANGES"].icon);
-			setReviewTypeRequestText(mapping["REQUEST_CHANGES"].text);
+			setReviewTypeApproval(mapping["APPROVED"]);
+			setReviewTypeRequest(mapping["REQUEST_CHANGES"]);
 		}
-	};
+	});
 
-	const submitReview = async user_click => {
-		let reviewType;
-		if (user_click === "Approve") {
-			reviewType = "APPROVE";
-		} else if (user_click === "Unapprove") {
-			reviewType = "UNAPPROVE";
-		} else if (user_click === "Request Changes") {
-			reviewType = "REQUEST_CHANGES";
-		} else if (user_click === "Changes Requested") {
-			reviewType = "CHANGES_REQUESTED";
-		}
+	const [reviewTypeApproval, setReviewTypeApproval] = useState<{
+		icon: string;
+		text: string;
+		requestedState: string;
+	}>(mapping["UNAPPROVED"]);
+	const [reviewTypeRequest, setReviewTypeRequest] = useState<{
+		icon: string;
+		text: string;
+		requestedState: string;
+	}>(mapping["REQUEST_CHANGES"]);
 
-		if (!props.pullRequest.viewerDidAuthor) {
-			await dispatch(
-				api({
-					method: "submitReview",
-					params: {
-						eventType: reviewType,
-						pullRequestId: props.pullRequest.id,
-					},
-				})
-			);
-		}
+	const submitReview = async (value: string) => {
+		dispatch(
+			api({
+				method: "submitReview",
+				params: {
+					eventType: value,
+					pullRequestId: props.pullRequest.id,
+				},
+			})
+		);
 	};
 
 	return (
 		<div>
 			<span>
-				<Icon //needs to change to unapprove thumbs down if already approved & needs to not be available if it's their own PR
-					name={reviewTypeApprovalIcon} //name of the icon to be shown to user; can be either thumbsup or thumbsdown
-					title={reviewTypeApprovalText} //text that shows to user when they hover, can be either Approve of Unapprove
-					trigger={["hover"]}
-					delay={1}
-					placement="bottom"
+				<Button
+					disabled={props.pullRequest.viewerDidAuthor}
 					onClick={e => {
-						submitReview(reviewTypeApprovalText);
+						submitReview(reviewTypeApproval.requestedState);
 					}}
-				/>
+				>
+					<Icon //needs to change to unapprove thumbs down if already approved & needs to not be available if it's their own PR
+						name={reviewTypeApproval.icon} //name of the icon to be shown to user; can be either thumbsup or thumbsdown
+						title={reviewTypeApproval.text} //text that shows to user when they hover, can be either Approve of Unapprove
+						trigger={["hover"]}
+						delay={1}
+						placement="bottom"
+					/>
+				</Button>
 			</span>
 			<span>
 				<Icon // if it's the person's own PR, they cannot request changes, should be grayed out. If changes are requested, it should show that
-					name={reviewTypeRequestIcon}
-					title={reviewTypeRequestText} //text that shows to user when hover, can be either Changes Requested or Request Changes
+					name={reviewTypeRequest.icon}
+					title={reviewTypeRequest.text} //text that shows to user when hover, can be either Changes Requested or Request Changes
 					trigger={["hover"]}
 					delay={1}
 					placement="bottom"
 					onClick={e => {
-						submitReview(reviewTypeRequestText);
+						submitReview(reviewTypeRequest.requestedState);
 					}}
 				/>
 			</span>
