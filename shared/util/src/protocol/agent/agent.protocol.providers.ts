ece112df3fba65efadfb794d3726493217c1d201
Marcelo Bukowski de Farias <mfarias@newrelic.com>
2023-03-16 13:28:57 -0400

    Anomaly chart

--- shared/util/src/protocol/agent/agent.protocol.providers.ts
+++ shared/util/src/protocol/agent/agent.protocol.providers.ts
@@ -1375,6 +1375,7 @@ export interface GetMethodLevelTelemetryRequest {
 	/** contains the specific formatting of a metricTimesliceName for a golden metric type */
 	metricTimesliceNameMapping?: MetricTimesliceNameMapping;
 	since?: string;
+	timeseriesGroup?: string;
 }
 
 export interface GetEntityCountRequest {
