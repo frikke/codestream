7a2ab6604f20237df6a43c694057ce35f1f2368e
Renee Petit <renee.petit@gmail.com>
2023-03-20 12:08:53 -0700

    WarningBox for Merge Error works

--- shared/ui/Stream/PullRequests/Bitbucket/MergeScreen.tsx
+++ shared/ui/Stream/PullRequests/Bitbucket/MergeScreen.tsx
@@ -22,7 +22,7 @@ export const MergeScreen = (props: Props) => {
 	const [mergeSelection, setMergeSelection] = useState("Merge commit");
 	const [isCloseSourceBranch, setIsCloseSourceBranch] = useState(false);
 	const [isChecked, setIsChecked] = useState(false);
-	const [isError, setIsError] = useState(false);
+	const [mergeError, setMergeError] = useState("");
 	const options = ["merge_commit", "squash", "fast_forward"];
 
 	const handleChange = () => {
@@ -34,9 +34,11 @@ export const MergeScreen = (props: Props) => {
 			setIsCloseSourceBranch(true);
 		}
 	};
+
 	let result;
 
 	const mergePullRequest = async () => {
+		setMergeError("");
 		result = (await dispatch(
 			api({
 				method: "mergePullRequest",
@@ -49,96 +51,84 @@ export const MergeScreen = (props: Props) => {
 			})
 		)) as any;
 		if (result.payload.error) {
-			console.log(result.payload.error);
-			setIsError(true);
-			return result.payload.error;
+			setMergeError(result.payload.error);
 		} else {
-			setIsError(false);
-			return;
+			return props.onClose();
 		}
 	};
 
 	return (
 		<Modal translucent>
-			{isError ? (
-				<WarningBox items={[{ message: result.payload.error }]}></WarningBox>
-			) : (
-				<Dialog
-					narrow
-					title="Merge pull request"
-					onClose={() => {
-						props.onClose();
-					}}
-				>
-					<div className="standard-form">
-						<fieldset className="form-body">
-							<div id="controls">
-								<small title="Source">Destination: {props.pr.headRefName} </small>
-								<br></br>
-								<small title="Desitnation">Source: {props.pr.baseRefName} </small>
-								<div style={{ margin: "20px 0" }}>
-									<div className="controls">
-										<label>Commit message</label>
-										<input
-											autoFocus
-											placeholder="your commit message"
-											className="input-text control"
-											type="text"
-											name="message"
-											value={mergeMessage}
-											onChange={e => {
-												setMergeMessage(e.target.value);
-											}}
-										/>
+			<Dialog
+				narrow
+				title="Merge pull request"
+				onClose={() => {
+					props.onClose();
+				}}
+			>
+				<div className="standard-form">
+					<fieldset className="form-body">
+						<div id="controls">
+							{mergeError && <WarningBox items={[{ message: mergeError }]}></WarningBox>}
+							<small title="Source">Destination: {props.pr.headRefName} </small>
+							<br></br>
+							<small title="Desitnation">Source: {props.pr.baseRefName} </small>
+							<div style={{ margin: "20px 0" }}>
+								<div className="controls">
+									<label>Commit message</label>
+									<input
+										autoFocus
+										placeholder="your commit message"
+										className="input-text control"
+										type="text"
+										name="message"
+										value={mergeMessage}
+										onChange={e => {
+											setMergeMessage(e.target.value);
+										}}
+									/>
 
-										<InlineMenu
-											items={[
-												{
-													label: "Merge commit",
-													key: "merge_commit",
-													action: () => {
-														setMergeMethod("merge_commit"), setMergeSelection("Merge commit");
-													},
+									<InlineMenu
+										items={[
+											{
+												label: "Merge commit",
+												key: "merge_commit",
+												action: () => {
+													setMergeMethod("merge_commit"), setMergeSelection("Merge commit");
 												},
-												{
-													label: "Squash",
-													key: "squash",
-													action: () => {
-														setMergeMethod("squash"), setMergeSelection("Squash");
-													},
+											},
+											{
+												label: "Squash",
+												key: "squash",
+												action: () => {
+													setMergeMethod("squash"), setMergeSelection("Squash");
 												},
-												{
-													label: "Fast forward",
-													key: "fast_forward",
-													action: () => {
-														setMergeMethod("fast_forward"), setMergeSelection("Fast forward");
-													},
+											},
+											{
+												label: "Fast forward",
+												key: "fast_forward",
+												action: () => {
+													setMergeMethod("fast_forward"), setMergeSelection("Fast forward");
 												},
-											]}
-										>
-											{mergeSelection}
-										</InlineMenu>
-										<div style={{ height: "10px" }} />
-									</div>
-									<div>
-										<label>
-											<input type="checkbox" checked={isChecked} onChange={handleChange} />
-											Close source branch
-										</label>
-									</div>
+											},
+										]}
+									>
+										{mergeSelection}
+									</InlineMenu>
+									<div style={{ height: "10px" }} />
 								</div>
-								<Button
-									onClick={() => {
-										mergePullRequest(), props.onClose();
-									}}
-								>
-									Merge
-								</Button>
+								<div>
+									<label>
+										<input type="checkbox" checked={isChecked} onChange={handleChange} />
+										Close source branch
+									</label>
+								</div>
 							</div>
-						</fieldset>
-					</div>
-				</Dialog>
-			)}
+							<Button onClick={mergePullRequest}>Merge</Button>
+						</div>
+					</fieldset>
+				</div>
+			</Dialog>
 		</Modal>
 	);
 };
