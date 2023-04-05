ccec949751f02e182ab882a0d16d5d7bfd59f7d8
Eric Jones <etjones319@gmail.com>
2023-04-05 12:20:55 -0700

    cleanup

--- shared/ui/Stream/CLMSettings.tsx
+++ shared/ui/Stream/CLMSettings.tsx
@@ -67,11 +67,6 @@ export const CLMSettings = () => {
 		}
 	`;
 
-	const explanatoryCopy = `These settings control how CodeStream determines whether or not a method’s
-	performance is anomalous. If you’re not seeing anomalies, decrease the thresholds.
-	Particularly the “minimum change”. If you’re seeing too many false positives,
-	increase the thresholds.`;
-
 	useDidMount(() => {
 		const entityGuid = derivedState?.activeO11y?.[derivedState?.currentO11yRepoId || ""];
 
